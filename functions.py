3a8a3f2f2dbfb6edee4a3dee55c1ae47d48a45de
ozoin <dreka655@gmail.com>
2023-04-19 21:57:19 +0200

    commit

--- functions.py
+++ functions.py
@@ -1,5 +1,28 @@
-def simple_function(x):
-    return x + 1
 
-def quadratic_function(x):
-    return x + x**2
+
+def test_function_1var(x):
+    return x**2+1
+def test_function_2var(x):
+    return x[0]**3+3*x[1]+10
+
+def quartic(x):
+    return x**4 - 3*x**2 + 2*x + 1
+
+def fifth_degree(x):
+    return 2*x**5 - 5*x**4 + 3*x**3 + x**2 - 2*x + 1
+
+def exponential_plus_c(x):
+    c = 3
+    return math.exp(x) + c
+
+def rational(x):
+    return (x**3 - x**2 + 1)/(x - 1)
+
+def third_degree(x):
+    return -7*x**2+0.5*x-7
+
+def fun_2vars(x):
+    return 0.5*(x[0] - 4.5)**2 + 2.5*(x[1] - 2.3)**2
+
+def cos_fun(x):
+    return np.cos(x)-2*x**3
