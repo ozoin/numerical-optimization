85c1192010da58497169552187b11e9fbad6ad64
ozoin <dreka655@gmail.com>
2023-04-20 15:58:12 +0200

    commit

--- functions.py
+++ functions.py
@@ -19,10 +19,21 @@ def rational(x):
     return (x**3 - x**2 + 1)/(x - 1)
 
 def third_degree(x):
-    return -7*x**2+0.5*x-7
+    return x**3 - 5*x - 9
 
 def fun_2vars(x):
     return 0.5*(x[0] - 4.5)**2 + 2.5*(x[1] - 2.3)**2
 
 def cos_fun(x):
     return np.cos(x)-2*x**3
+
+def cos_exp(x):
+    return 3*math.cos(x) - math.exp(x)
+
+
+A = np.asmatrix(np.random.rand(100,3))
+b = np.asmatrix(np.random.rand(100,1))
+#Defining the Function
+def quadratic_problem_iii(x): #x is 3d point e.g [1,1,1], Returns a scalar function value
+    point = np.asmatrix(x)
+    return np.asscalar(0.5*np.dot((np.dot(A,point.T) - b).T,(np.dot(A,point.T) - b)))
