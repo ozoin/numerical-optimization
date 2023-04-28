1b060494acaefeef005f4091cb570f8a2f409d1a
ozoin <dreka655@gmail.com>
2023-04-28 15:14:07 +0200

    commit

--- functions.py
+++ functions.py
@@ -1,34 +1,46 @@
+import numpy as np
+import math
 
 
-def test_function_1var(x):
-    return x**2+1
-def test_function_2var(x):
-    return x[0]**3+3*x[1]+10
-
+#===EX1 PROBLEMS | 1 VARIABLEM===
 def quartic(x):
+    #correct for 0, but converges infinitely
     return x**4 - 3*x**2 + 2*x + 1
 
-def fifth_degree(x):
-    return 2*x**5 - 5*x**4 + 3*x**3 + x**2 - 2*x + 1
+def fourth_degree_polynomial(x):
+    #correct for point 1. but converges infinitely
+    return 2*x**4 + 5*x**3 - 3*x**2 + 7*x - 8
 
-def exponential_plus_c(x):
-    c = 3
-    return math.exp(x) + c
-
-def rational(x):
-    return (x**3 - x**2 + 1)/(x - 1)
-
+def sixth_degree_polynomial(x):
+    #correct for point 1
+    return x**6 - 2*x**5 + 3*x**4 + 4*x**3 - 5*x**2 + 6*x - 7
+def booth_func(x):
+    #correct for point 0, but converges infinetly
+    return (x + 2*x**2 - 7)**2 + (2*x + x**2 - 5)**2
 def third_degree(x):
+    #correct for point 0, but converges infinetly
     return x**3 - 5*x - 9
-
-def fun_2vars(x):
-    return 0.5*(x[0] - 4.5)**2 + 2.5*(x[1] - 2.3)**2
-
 def cos_fun(x):
+    #correct for point 1.
     return np.cos(x)-2*x**3
 
-def cos_exp(x):
-    return 3*math.cos(x) - math.exp(x)
+#===EX5 PROBLEMS | 2 VARIABLES===
+def fun_2vars(x):
+    #correct for point 0,0
+    return 0.5*(x[0] - 4.5)**2 + 2.5*(x[1] - 2.3)**2
+
+
+
+def test(x):
+    return x**4+5*x**2+2
+
+sixth_degree_polynomial_x0 = [1]
+fourth_degree_polynomial_x0 = [1]
+quartic_x0 = [1]
+booth_func_x0 = [0]
+cos_fun_x0 = [1]
+third_degree_x0 = [0] 
+#====
 
 
 A = np.asmatrix(np.random.rand(100,3))
